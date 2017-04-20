0cbdfb889ed17c139d5c95394349d3a79c1833d1
Wendy Knox Everette <weverett@gmu.edu>
2017-04-20 08:25:03 -0400

    cleaner
    Python 2.7 doesn’t require explicit strip of newlines! yay python

--- homeworks/users.py
+++ homeworks/users.py
@@ -4,7 +4,7 @@
 users = {}
 print "enter the path of a shadow file:"
 myFile = raw_input();
-lines = [line.strip() for line in open(myFile)]
+lines = [open(myFile)]
 for line in lines:
     if (line):
         parts = line.split(":")
