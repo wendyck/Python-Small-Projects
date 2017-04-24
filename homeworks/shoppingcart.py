5f0d93c1f081338737ccf630f5a680e9ad6a6c25
Wendy Knox Everette <weverett@gmu.edu>
2017-04-24 18:21:46 -0400

    objects

---
+++ homeworks/shoppingcart.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+class ShoppingCart(object):
+
+    cart_items = []
+
+    def __init__(self):
+        self.name = "whizz bang shopping cart"
+        
+
+    def add_item(self, Inventory):
+        self.cart_items.append(Inventory);
+
+    def print_cart(self):
+        print "cart contents:"
+        print "-" * 30
+        total = 0.0
+        for widget in self.cart_items:
+            name = widget.get_name();
+            quantity = widget.get_quantity();
+            price = widget.get_price();
+            num = widget.get_quantity();
+            subtotal = price * num;
+            total += subtotal;
+            print "Item: ", name, ", ", quantity, " at $", price, " each: $", subtotal;
+        print "Total Price $", total
+
+class Inventory(object):
+    def __init__ (self, item_name, item_price = 0.0, item_quantity=1):
+        self.item_name = item_name;
+        self.item_price = item_price;
+        self.item_quantity = item_quantity;
+    
+    def get_price(self):
+        return self.item_price;
+    def get_quantity(self):
+        return self.item_quantity;
+    def set_quantity(self, increase):
+        self.item_quantity += increase;
+
+    def get_name(self):
+        return self.item_name;
+
+        
+        
+#inventory object that stores item names, prices, and quantities
+        
+shoppingcart = ShoppingCart();
+
+iphone = Inventory("iPhone", 2.0, 10)
+name = iphone.get_name();
+shoppingcart.add_item(iphone);
+
+android = Inventory("Samsung Galaxy", 400.0, 1)
+
+shoppingcart.add_item(android);
+
+shoppingcart.print_cart();
